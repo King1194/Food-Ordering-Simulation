368195286670a71989bb84166b5010ec9a84c458
Ayush Srivastava <99962358+King1194@users.noreply.github.com>
2023-04-19 18:54:21 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Customer
+{
+    private String customerName;
+
+
+    Customer(String name)
+    {
+        this.customerName = name;
+    }
+
+
+    public String getCustomerName()
+    {
+        return customerName;
+    }
+
+}
+
